c90c217e6bea757d86f696f5ffaafa2e0f9d9341
Admi335 <64468662+Admi335@users.noreply.github.com>
2020-06-13 23:48:51 +0200

    Delete command.js

--- command.js
+++
@@ -1,9 +0,0 @@
-/*class Command {
-    constructor(name, description, usage) {
-        this.name = name;
-        this.description = description;
-        this.usage = usage;
-    }
-}
-
-module.exports = Command;*/
